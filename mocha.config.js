9517aaabb393e6fb87f1a1483373ad01052494a2
Matt Hampton <matth@j5int.com>
2016-02-11 16:07:34 +0200

    Put in a stub for the 'document' global so that we can run the tests directly through node.

--- mocha.config.js
+++ mocha.config.js
@@ -3,4 +3,7 @@ global.expect = global.chai.expect;
 global.sinon = require('sinon');
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
+global.document = {
+  getElementById: function(a) { return null}
+}
 
